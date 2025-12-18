7c64f63355469ec0909f6a66ff54a758c08bfcdb
Lucas-132 <lucas.pollet135@gmail.com>
2025-12-18 16:17:05 +0100

    Added pull query for GET method in controller file.

--- backend/src/controllers/clientsController.js
+++ backend/src/controllers/clientsController.js
@@ -1,17 +1,22 @@
 import { pool } from "../config/db.js";
 
-export const getAllClients = (req,res) => {
-    res.status(200).send("You just fetched all clients.");
+export const getAllClients = async (req,res) => {
+    try{
+        const [rows] = await pool.query("SELECT * FROM clients");
+        res.status(200).json(rows);
+    } catch (error){
+        res.status(500).json({ error: error.message });
+    }
 };
 
-export const createClient = (req, res) => {
+export const createClient = async (req, res) => {
     res.status(201).json({message: "Client created"});
 };
 
-export const updateClient = (req, res) => {
+export const updateClient = async (req, res) => {
     res.status(200).json({message: "Client updated"});
 };
 
-export const deleteClient = (req, res) => {
+export const deleteClient = async (req, res) => {
     res.status(200).json({message: "Client deleted"});
 };
